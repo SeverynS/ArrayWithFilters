7cf14296a8efdf788fa2c830e7bc4a27886fc294
SeverynZ <s.zlochovsky@gmail.com>
2021-09-20 23:57:30 +0300

    Next generation

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,50 +12,37 @@ ruleKey == "name" and ruleValue == namei.
 Return the number of items that match the given rule.*/
 public class Test {
     public static void main(String[] args) {
-        String[] arr = new String[6];
-        arr[0] = "phone, black, xiaomi";
-        arr[1] = "phone, white, iphone";
-        arr[2] = "pc, silver, dell";
-        arr[3] = "laptop, pink, macbook";
-        arr[4] = "headphones, gold, airpods";
-        arr[5] = "phone, gold, iphone";
 
-        System.out.println(parameterFiltering(arr, "color", "black"));
-        System.out.println(parameterFiltering(arr, "type", "phone"));
-        System.out.println(parameterFiltering(arr, "name", "iphone"));
-        System.out.println(parameterFiltering(arr, "color", "gold"));
+        List<List<String>> inputData = new ArrayList<>(Arrays.asList(
+                Collections.singletonList("phone, black, xiaomi"),
+                Collections.singletonList("phone, white, iphone"),
+                Collections.singletonList("pc, silver, dell"),
+                Collections.singletonList("laptop, pink, macbook"),
+                Collections.singletonList("headphones, gold, airpods"),
+                Collections.singletonList("phone, gold, iphone")
+        ));
+
+        System.out.println(parameterFiltering(inputData, "color", "black"));
+        System.out.println(parameterFiltering(inputData, "type", "phone"));
+        System.out.println(parameterFiltering(inputData, "name", "iphone"));
+        System.out.println(parameterFiltering(inputData, "color", "gold"));
     }
 
-    private static int parameterFiltering(String[] arr, String ruleKey, String ruleValue) {
+    private static int parameterFiltering(List<List<String>> arr, String ruleKey, String ruleValue) {
         int result = 0;
-        List<String> stringList = stringSplitting(arr);
-        HashMap<String, String> mapTemp = new HashMap<>();
-        List<HashMap> listMaps = new ArrayList<>();
-        for (int i = 0; i < stringList.size(); i += 3) {
-            mapTemp.put("type", stringList.get(i));
-            mapTemp.put("color", stringList.get(i + 1));
-            mapTemp.put("name", stringList.get(i + 2));
-            listMaps.add(new HashMap<>(mapTemp));
-        }
-
-        for (HashMap hashMap : listMaps) {
-            mapTemp = hashMap;
-            for (Map.Entry<String, String> entry : mapTemp.entrySet()) {
-                if (entry.getKey().equals(ruleKey) && entry.getValue().equals(ruleValue)) {
-                    result++;
-                }
+        boolean type = ruleKey.equals("type");
+        boolean color = ruleKey.equals("color");
+        boolean name = ruleKey.equals("name");
+        for (List l : arr) {
+            String s = l.toString();
+            String s2 = s.substring(1, s.length() - 1);
+            String[] strArr = s2.split(", ");
+            if (type && ruleValue.equals(strArr[0])
+                    || color && ruleValue.equals(strArr[1])
+                    || name && ruleValue.equals(strArr[2])) {
+                result++;
             }
         }
         return result;
-    }
-
-    private static List<String> stringSplitting(String[] arr) {
-        String[] temp;
-        List<String> stringList = new LinkedList<>();
-        for (String s : arr) {
-            temp = s.split(", ", 3);
-            stringList.addAll(Arrays.asList(temp));
-        }
-        return stringList;
     }
 }
